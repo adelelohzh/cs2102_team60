c030ceb363eb2437e3e862c4fd60e138f928f008
Valencia <valencia@comp.nus.edu.sg>
2021-10-17 16:22:42 +0800

    edited schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,130 @@
---SQL Command to create application database schema--
+--CREATE DATABASE
+
+DROP TABLE IF EXISTS Employees, Junior, Booker, Senior, Manager, Meeting_Rooms, Departments, Health_Declaration, Sessions, Works_In, Located_In, Joins, Books, Approves, Updates;
+;
+
+CREATE TABLE Employees (
+	eid INTEGER,
+	ename TEXT NOT NULL,
+	email TEXT UNIQUE NOT NULL,
+	contact VARCHAR(100) NOT NULL,
+	resigned_date DATE,
+    PRIMARY KEY (eid)
+);
+
+CREATE TABLE Junior (
+	eid INTEGER PRIMARY KEY REFERENCES Employees(eid) ON DELETE CASCADE
+);
+
+CREATE TABLE Booker (
+    eid INTEGER PRIMARY KEY REFERENCES Employees(eid) ON DELETE CASCADE
+);
+
+CREATE TABLE Senior (
+    eid INTEGER PRIMARY KEY REFERENCES Booker(eid) ON DELETE CASCADE
+);
+
+CREATE TABLE Manager (
+    eid INTEGER PRIMARY KEY REFERENCES Booker(eid) ON DELETE CASCADE
+);
+
+CREATE TABLE Health_Declaration (
+    eid INTEGER,
+    date DATE,
+    temp FLOAT,
+    fever BOOLEAN,
+    PRIMARY KEY(eid, date),
+    FOREIGN KEY (eid) REFERENCES Employees(eid) ON DELETE CASCADE
+); 
+
+CREATE TABLE Departments (
+    did INTEGER,
+    dname VARCHAR(30),
+    PRIMARY KEY (did)
+);
+
+CREATE TABLE Sessions (
+	room INTEGER
+	floor INTEGER,
+	time VARCHAR(5),
+	date DATE,
+	PRIMARY KEY (room, floor, time, date),
+	FOREIGN KEY (room) REFERENCES Meeting_Rooms (room) ON DELETE CASCADE, 
+	FOREIGN KEY (floor) REFERENCES Meeting_Rooms (floor) ON DELETE CASCADE
+);
+
+CREATE TABLE Meeting_Rooms (
+	floor INTEGER,
+	room INTEGER,
+	rname VARCHAR(10),
+	PRIMARY KEY (floor, room)
+);
+
+CREATE TABLE Works_In(
+    eid INTEGER REFERENCES Employees,
+    did INTEGER NOT NULL,
+    PRIMARY KEY (eid),
+    FOREIGN KEY (did) REFERENCES Departments
+);
+
+CREATE TABLE Located_In (
+    room INTEGER,
+	floor INTEGER,
+	did INTEGER,
+	PRIMARY KEY (room, floor),
+	FOREIGN KEY (did) REFERENCES Departments,
+	FOREIGN KEY (room) REFERENCES Meeting_Rooms (room) ON DELETE CASCADE, 
+	FOREIGN KEY (floor) REFERENCES Meeting_Rooms (floor) ON DELETE CASCADE
+);
+
+CREATE TABLE Joins (
+        	eid INTEGER REFERENCES Employees
+	room VARCHAR(10) NOT NULL
+	floor INTEGER NOT NULL
+	time VARCHAR(5) NOT NULL
+	date DATE NOT NULL
+	PRIMARY KEY (eid, room, floor, time, date),
+	FOREIGN KEY (room) REFERENCES Sessions (room) ON DELETE CASCADE, 
+	FOREIGN KEY (floor) REFERENCES Sessions (floor) ON DELETE CASCADE,
+	FOREIGN KEY (time) REFERENCES Sessions (time) ON DELETE CASCADE, 
+	FOREIGN KEY (date) REFERENCES Sessions (date) ON DELETE CASCADE
+ );
+
+CREATE TABLE Books (
+	eid INTEGER NOT NULL, 
+    room INTEGER,
+	floor INTEGER,
+	time VARCHAR(5),
+	date DATE,
+	PRIMARY KEY (room, floor, time, date),
+	FOREIGN KEY (room) REFERENCE Sessions (room) ON DELETE CASCADE, 
+	FOREIGN KEY (floor) REFERENCE Sessions (floor) ON DELETE CASCADE,
+	FOREIGN KEY (time) REFERENCE Sessions (time) ON DELETE CASCADE, 
+	FOREIGN KEY (date) REFERENCE Sessions (date) ON DELETE CASCADE,
+    FOREIGN KEY (eid) REFERENCE Booker (eid) ON DELETE CASCADE
+);
+
+CREATE TABLE Approves(
+    eid INTEGER NOT NULL,
+	room VARCHAR(10),
+	floor INTEGER,
+	time VARCHAR(5),
+	date DATE,
+	PRIMARY KEY (room, floor, time, date),
+	FOREIGN KEY (room) REFERENCE Sessions (room) ON DELETE CASCADE, 
+	FOREIGN KEY (floor) REFERENCE Sessions (floor) ON DELETE CASCADE,
+	FOREIGN KEY (time) REFERENCE Sessions (time) ON DELETE CASCADE, 
+	FOREIGN KEY (date) REFERENCE Sessions (date) ON DELETE CASCADE,
+    FOREIGN KEY (eid) REFERENCE Manager (eid) ON DELETE CASCADE
+);
+ 
+CREATE TABLE Updates (
+    date DATE, 
+    new_cap INTEGER,
+	room INTEGER NOT NULL,
+	floor INTEGER NOT NULL,
+	eid INTEGER ,
+    PRIMARY KEY(date, room, floor, eid),
+	FOREIGN KEY (eid) REFERENCES Manager ON DELETE CASCADE,
+    FOREIGN KEY (room,floor) REFERENCES Meeting Rooms ON DELETE CASCADE
+ );
