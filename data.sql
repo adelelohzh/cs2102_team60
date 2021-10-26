9134f7b677a0fafb4bf083a934eb9bceee5234b7
Jing Hao Ong <jinghaoong@gmail.com>
2021-10-26 15:09:32 +0800

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,1 +1,469 @@
---SQL commands to load data--
+-- SQL commands to load data
+
+--Employees (80)
+INSERT INTO Employees (eid, ename, email, contact, resigned_date) VALUES
+(1, 'Malinde McGeraghty', 'mmcgeraghty0@facebook.com', '7541822913', NULL),
+(2, 'Carla Deviney', 'cdeviney1@foxnews.com', '5621820619', NULL),
+(3, 'Eugine Kingswood', 'ekingswood2@elpais.com', '5631774350', NULL),
+(4, 'Wallace Ace', 'wace3@instagram.com', '6411296552', NULL),
+(5, 'Riordan Cordingley', 'rcordingley4@rediff.com', '6713180174', NULL),
+(6, 'Ozzie Cartner', 'ocartner5@ebay.com', '2657941990', NULL),
+(7, 'Kaleb Frankton', 'kfrankton6@irs.gov', '6867698995', NULL),
+(8, 'Raviv Willison', 'rwillison7@indiatimes.com', '1406008645', NULL),
+(9, 'Caren Duns', 'cduns8@ask.com', '2696970158', NULL),
+(10, 'Rozanne Fincke', 'rfincke9@irs.gov', '3716324443', NULL),
+(11, 'Schuyler Rabier', 'srabiera@netscape.com', '4193537483', NULL),
+(12, 'Myranda Woolmington', 'mwoolmingtonb@europa.eu', '8007733933', NULL),
+(13, 'Nicky Haggarty', 'nhaggartyc@oaic.gov.au', '7181765140', NULL),
+(14, 'Brew Rutherfoord', 'brutherfoordd@ycombinator.com', '1432244057', NULL),
+(15, 'Randie Blount', 'rblounte@nydailynews.com', '1134245012', NULL),
+(16, 'Dacie Le Brum', 'dlef@upenn.edu', '6471291907', NULL),
+(17, 'Beatrice Bouts', 'bboutsg@wix.com', '8462445932', NULL),
+(18, 'Lind Lickess', 'llickessh@mysql.com', '2603631989', NULL),
+(19, 'Rodrick Derisly', 'rderislyi@adobe.com', '7035844249', NULL),
+(20, 'Zechariah Colleford', 'zcollefordj@abc.net.au', '4215664651', NULL),
+(21, 'Dall Brodeau', 'dbrodeauk@google.co.jp', '8376940655', NULL),
+(22, 'Micheal Scanlin', 'mscanlinl@google.pl', '5493120218', NULL),
+(23, 'Dwight Eefting', 'deeftingm@delicious.com', '2723730118', NULL),
+(24, 'Base Lowrance', 'blowrancen@livejournal.com', '2783250293', NULL),
+(25, 'Nomi Caven', 'ncaveno@forbes.com', '6774309749', NULL),
+(26, 'Vida Gray', 'vgrayp@dedecms.com', '3849359368', NULL),
+(27, 'Ezmeralda Melan', 'emelanq@parallels.com', '5806476095', NULL),
+(28, 'Kandy Liveing', 'kliveingr@opera.com', '9911569631', NULL),
+(29, 'Ashly Erie', 'aeries@gizmodo.com', '9002000440', NULL),
+(30, 'Xaviera Nisuis', 'xnisuist@cnbc.com', '8553719032', NULL),
+(31, 'Abran Kleinhausen', 'akleinhausenu@t-online.de', '8328087354', NULL),
+(32, 'Chancey Basili', 'cbasiliv@mapquest.com', '4153702799', NULL),
+(33, 'Tadeas Walmsley', 'twalmsleyw@aol.com', '5958749010', NULL),
+(34, 'Rikki Yates', 'ryatesx@usatoday.com', '7126728535', NULL),
+(35, 'Tore Scyner', 'tscynery@elegantthemes.com', '6582698656', NULL),
+(36, 'Fred Hawlgarth', 'fhawlgarthz@cdbaby.com', '5366164988', NULL),
+(37, 'Ely Jozwik', 'ejozwik10@sogou.com', '1848462307', NULL),
+(38, 'Bernadette Findon', 'bfindon11@chicagotribune.com', '4199446517', NULL),
+(39, 'Nancee Tomaszynski', 'ntomaszynski12@amazon.co.uk', '5851753727', NULL),
+(40, 'Miran Byars', 'mbyars13@nifty.com', '7098517599', NULL),
+(41, 'Arlena Pape', 'apape14@sitemeter.com', '8016714085', NULL),
+(42, 'Dareen Meechan', 'dmeechan15@ucoz.com', '2695419090', NULL),
+(43, 'Allan Dwyr', 'adwyr16@uiuc.edu', '4816551234', NULL),
+(44, 'Evanne Krout', 'ekrout17@diigo.com', '4534417143', NULL),
+(45, 'Rennie Basler', 'rbasler18@deviantart.com', '8974738443', NULL),
+(46, 'Jojo Walewor', 'jwalework19@topsy.com', '1111587799', NULL),
+(47, 'Lilllie Kilbourne', 'lkilbourne1a@digg.com', '3658615190', NULL),
+(48, 'Kris Yirrell', 'kyirrell1b@tiny.cc', '3107853006', NULL),
+(49, 'Luca Rosenstein', 'lrosenstein1c@ycombinator.com', '8796693111', NULL),
+(50, 'Clarinda Sapseed', 'csapseed1d@bluehost.com', '1895950600', NULL),
+(51, 'Osbourn More', 'omore1e@umn.edu', '2708528870', NULL),
+(52, 'Avram Asals', 'aasals1f@sbwire.com', '7442065920', NULL),
+(53, 'Ody Ruppele', 'oruppele1g@csmonitor.com', '8497089385', NULL),
+(54, 'Chantalle Ciccetti', 'cciccetti1h@mlb.com', '9223138089', NULL),
+(55, 'Vania Santus', 'vsantus1i@geocities.com', '3788570681', NULL),
+(56, 'Valry Egel' , 'vegel1j@abc.net.au', '3634221332', NULL),
+(57, 'Penn Elijah', 'pelijah1k@51.la', '5465856470', NULL),
+(58, 'Phyllis Wyleman', 'pwyleman1l@wikispaces.com', '1183458467', NULL),
+(59, 'Bob Shemmans', 'bshemmans1m@washington.edu', '3521799526', NULL),
+(60, 'Holden Culter', 'hculter1n@yellowpages.com', '9415707054', NULL),
+(61, 'Candy Sellman', 'csellman1o@reuters.com', '9928496225', NULL),
+(62, 'Kort Wraith', 'kwraith1p@springer.com', '1765843994', NULL),
+(63, 'Aurlie Alfwy', 'aalfwy1q@wordpress.com', '2599224447', NULL),
+(64, 'Ricki Tarbatt', 'rtarbatt1r@joomla.org', '5977043196', NULL),
+(65, 'Harwell Leeb','hleeb1s@howstuffworks.com', '4128306429', NULL),
+(66, 'Babita Horwell', 'bhorwell1t@pinterest.com', '4761957147', NULL),
+(67, 'Porty Janous', 'pjanous1u@fastcompany.com', '5678547066', NULL),
+(68, 'Glenn Quinet', 'gquinet1v@g.co', '1431418333', NULL),
+(69, 'Ailina Capineer', 'acapineer1w@sina.com.cn', '4315426627', NULL),
+(70, 'Hyacinthia Bolle', 'hbolle1x@blogger.com', '2457789692', NULL),
+(71, 'Marlo Grigoryov', 'mgrigoryov1y@blogspot.com', '7079522617', NULL),
+(72, 'Jobyna Bockin', 'jbockin1z@blogger.com', '2297811314', NULL),
+(73, 'Myrvyn Renon', 'mrenon20@columbia.edu', '1999327906', NULL),
+(74, 'Janeen Burdon', 'jburdon21@forbes.com', '2102361906', NULL),
+(75, 'Slade Tyres', 'styres22@bloomberg.com', '7511024923', NULL),
+(76, 'Ryan Coghlan', 'rcoghlan23@barnesandnoble.com', '3989659577', NULL),
+(77, 'Jennilee Thonger', 'jthonger24@instagram.com', '8646802915', NULL),
+(78, 'Emelen Brokenbrow', 'ebrokenbrow25@example.com', '8645540009', NULL),
+(79, 'Rockey Della Scala', 'rdella26@dailymotion.com', '5317988081', NULL),
+(80, 'Pierrette Febry', 'pfebry27@addthis.com', '4649531357', NULL);
+
+--Junior (56)
+INSERT INTO Junior (eid) VALUES
+(4), (5), (6), (7), (8), (9), (10),
+(14), (15), (16), (17), (18), (19), (20),
+(24), (25), (26), (27), (28), (29), (30),
+(34), (35), (36), (37), (38), (39), (40),
+(44), (45), (46), (47), (48), (49), (50),
+(54), (55), (56), (57), (58), (59), (60),
+(74), (75), (76), (77), (78), (79), (80);
+
+--Booker (24)
+INSERT INTO Booker (eid) VALUES
+(1), (2), (3),
+(11), (12), (13),
+(21), (22), (23),
+(31), (32), (33),
+(41), (42), (43),
+(51), (52), (53),
+(61), (62), (63),
+(71), (72), (73);
+
+--Manager (8)
+INSERT INTO Manager (eid) VALUES 
+(1), (11), (21), (31), (41), (51), (61), (71);
+
+--Senior (16)
+INSERT INTO Senior (eid) VALUES 
+(2), (3), (12), (13), 
+(22), (23), (32), (33), 
+(42), (43), (52), (53), 
+(62), (63), (72), (73);
+
+--Departments (8)
+INSERT INTO Departments (did, dname) VALUES
+(1, 'Finance'),
+(2, 'HR'),
+(3, 'Public Relations'),
+(4, 'Sales'),
+(5, 'Procurement'),
+(6, 'Marketing'),
+(7, 'Operations'),
+(8, 'Information Technology');
+
+--Meeting Rooms (24)
+INSERT INTO Meeting_Rooms (room, floor, rname) VALUES
+(1, 1, 'Noodlin'),
+(2, 1, 'Brainstorm'),
+(3, 1, 'Learning'),
+(1, 2, 'Pressure'),
+(2, 2, 'Boredom'),
+(3, 2, 'Minds'),
+(1, 3, 'Mission'),
+(2, 3, 'Discussion'),
+(3, 3, 'Cranium'),
+(1, 4, 'Thinking'),
+(2, 4, 'Chamber'),
+(3, 4, 'Focus'),
+(1, 5, 'Ilulu'),
+(2, 5, 'Deep'),
+(3, 5, 'Strategy'),
+(1, 6, 'Comms'),
+(2, 6, 'Planning'),
+(3, 6, 'Devising'),
+(1, 7, 'Planning'),
+(2, 7, 'Goals'),
+(3, 7, 'Striving'),
+(1, 8, 'Technology'),
+(2, 8, 'Info'),
+(3, 8, 'Amazing');
+
+--Sessions (11)
+INSERT INTO Sessions (room, floor, time, date) VALUES
+(1, 1, 9, '2021-10-15'),
+(2, 1, 10, '2021-10-15'),
+(3, 1, 9, '2021-10-15'),
+(1, 2, 13, '2021-10-15'),
+(3, 3, 13, '2021-10-16'),
+(1, 4, 14, '2021-10-16'),
+(2, 4, 11, '2021-10-23'),
+(3, 5, 9, '2021-10-24'),
+(2, 6, 9, '2021-10-25'),
+(1, 7, 9, '2021-10-25'),
+(3, 8, 9, '2021-10-26');
+
+--Health Declarations (80)
+INSERT INTO Health_Declaration (eid, date, temp, fever) VALUES
+(1, '2021-09-14', 35.4, FALSE),
+(2, '2021-09-09', 38, TRUE),
+(3, '2021-08-09', 37.7, TRUE),
+(4, '2021-08-23', 39.4, TRUE),
+(5, '2021-07-20', 37.5, FALSE),
+(6, '2021-10-25', 36.1, FALSE),
+(7, '2021-09-21', 37.5, FALSE),
+(8, '2021-09-10', 35.4, FALSE),
+(9, '2021-07-17', 39.4, TRUE),
+(10, '2021-06-13', 39.4, TRUE),
+(11, '2021-06-08', 35.6, FALSE),
+(12, '2021-07-10', 36.6, FALSE),
+(13, '2021-10-26', 38.3, TRUE),
+(14, '2021-09-16', 37.8, TRUE),
+(15, '2021-10-26', 38.4, TRUE),
+(16, '2021-10-19', 36.3, FALSE),
+(17, '2021-06-17', 38.8, TRUE),
+(18, '2021-07-05', 36.8, FALSE),
+(19, '2021-09-18', 37.5, FALSE),
+(20, '2021-08-22', 36.5, FALSE),
+(21, '2021-07-03', 36.2, FALSE),
+(22, '2021-08-04', 39.4, TRUE),
+(23, '2021-08-23', 35.1, FALSE),
+(24, '2021-07-04', 36.2, FALSE),
+(25, '2021-08-30', 39.3, TRUE),
+(26, '2021-08-01', 35.7, FALSE),
+(27, '2021-09-03', 39.5, TRUE),
+(28, '2021-09-10', 38.8, TRUE),
+(29, '2021-06-05', 35.5, FALSE),
+(30, '2021-08-12', 37.8, TRUE),
+(31, '2021-10-24', 38.7, TRUE),
+(32, '2021-10-18', 39.9, TRUE),
+(33, '2021-06-15', 35.9, FALSE),
+(34, '2021-07-20', 37.4, FALSE),
+(35, '2021-06-19', 36.5, FALSE),
+(36, '2021-10-19', 35.5, FALSE),
+(37, '2021-07-17', 36.6, FALSE),
+(38, '2021-10-04', 35.9, FALSE),
+(39, '2021-08-22', 39.5, TRUE),
+(40, '2021-08-17', 37.9, TRUE),
+(41, '2021-07-09', 38.6, TRUE),
+(42, '2021-06-06', 39.5, TRUE),
+(43, '2021-07-05', 37.4, FALSE),
+(44, '2021-09-29', 35.8, FALSE),
+(45, '2021-09-04', 37.6, TRUE),
+(46, '2021-07-17', 37.3, FALSE),
+(47, '2021-07-21', 37.8, TRUE),
+(48, '2021-07-16', 39.8, TRUE),
+(49, '2021-08-24', 40, TRUE),
+(50, '2021-10-13', 35.5, FALSE),
+(51, '2021-09-11', 35.8, FALSE),
+(52, '2021-06-05', 36.3, FALSE),
+(53, '2021-10-13', 35.2, FALSE),
+(54, '2021-09-28', 38.4, TRUE),
+(55, '2021-06-28', 38.0, TRUE),
+(56, '2021-09-19', 36.3, FALSE),
+(57, '2021-09-27', 37.2, FALSE),
+(58, '2021-08-27', 39, TRUE),
+(59, '2021-09-18', 35.4, FALSE),
+(60, '2021-06-22', 35.4, FALSE),
+(61, '2021-06-04', 39.7, TRUE),
+(62, '2021-09-17', 38.2, TRUE),
+(63, '2021-09-08', 38.8, TRUE),
+(64, '2021-07-12', 38.6, TRUE),
+(65, '2021-06-24', 39.7, TRUE),
+(66, '2021-08-07', 35.5, FALSE),
+(67, '2021-09-05', 35.4, FALSE),
+(68, '2021-10-11', 36.1, FALSE),
+(69, '2021-08-10', 37.4, FALSE),
+(70, '2021-10-24', 37.9, TRUE),
+(71, '2021-09-23', 39.2, TRUE),
+(72, '2021-09-24', 36.8, FALSE),
+(73, '2021-07-23', 37, FALSE),
+(74, '2021-08-09', 36.5, FALSE),
+(75, '2021-09-11', 38.7, TRUE),
+(76, '2021-08-27', 36.9, FALSE),
+(77, '2021-09-03', 39.6, TRUE),
+(78, '2021-07-02', 36.1, FALSE),
+(79, '2021-08-02', 35.7, FALSE),
+(80, '2021-09-02', 39.4, TRUE);
+
+--Works_In (80)
+INSERT INTO Works_In (eid, did) VALUES 
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1),
+(6, 1),
+(7, 1),
+(8, 1),
+(9, 1),
+(10, 1),
+(11, 2),
+(12, 2),
+(13, 2),
+(14, 2),
+(15, 2),
+(16, 2),
+(17, 2),
+(18, 2),
+(19, 2),
+(20, 2),
+(21, 3),
+(22, 3),
+(23, 3),
+(24, 3),
+(25, 3),
+(26, 3),
+(27, 3),
+(28, 3),
+(29, 3),
+(30, 4),
+(31, 4),
+(32, 4),
+(33, 4),
+(34, 4),
+(35, 4),
+(36, 4),
+(37, 4),
+(38, 4),
+(39, 4),
+(40, 4),
+(41, 5),
+(42, 5),
+(43, 5),
+(44, 5),
+(45, 5),
+(46, 5),
+(47, 5),
+(48, 5),
+(49, 5),
+(50, 5),
+(51, 6),
+(52, 6),
+(53, 6),
+(54, 6),
+(55, 6),
+(56, 6),
+(57, 6),
+(58, 6),
+(59, 6),
+(60, 6),
+(61, 7),
+(62, 7),
+(63, 7),
+(64, 7),
+(65, 7),
+(66, 7),
+(67, 7),
+(68, 7),
+(69, 7),
+(70, 7),
+(71, 8),
+(72, 8),
+(73, 8),
+(74, 8),
+(75, 8),
+(76, 8),
+(77, 8),
+(78, 8),
+(79, 8),
+(80, 8);
+
+--Located_In (24)
+INSERT INTO Located_In (room, floor, did) VALUES 
+(1, 1, 1),
+(2, 1, 1),
+(3, 1, 1),
+(1, 2, 2),
+(2, 2, 2),
+(3, 2, 2),
+(1, 3, 3),
+(2, 3, 3),
+(3, 3, 3),
+(1, 4, 4),
+(2, 4, 4),
+(3, 4, 4),
+(1, 5, 5),
+(2, 5, 5),
+(3, 5, 5),
+(1, 6, 6),
+(2, 6, 6),
+(3, 6, 6),
+(1, 7, 7),
+(2, 7, 7),
+(3, 7, 7),
+(1, 8, 8),
+(2, 8, 8),
+(3, 8, 8);
+
+--Updates (24)
+INSERT INTO Updates (date, new_cap, room, floor, eid) VALUES
+('2021-10-15', 4, 1, 1, 1),
+('2021-10-15', 4, 2, 1, 1),
+('2021-10-15', 4, 3, 1, 1),
+('2021-10-15', 4, 1, 2, 11),
+('2021-10-15', 4, 2, 2, 11),
+('2021-10-15', 4, 3, 2, 11),
+('2021-10-15', 4, 1, 3, 21),
+('2021-10-15', 4, 2, 3, 21),
+('2021-10-15', 4, 3, 3, 21),
+('2021-10-15', 4, 1, 4, 31),
+('2021-10-15', 4, 2, 4, 31),
+('2021-10-15', 4, 3, 4, 31),
+('2021-10-15', 4, 1, 5, 41),
+('2021-10-15', 4, 2, 5, 41),
+('2021-10-15', 4, 3, 5, 41),
+('2021-10-15', 4, 1, 6, 51),
+('2021-10-15', 4, 2, 6, 51),
+('2021-10-15', 4, 3, 6, 51),
+('2021-10-15', 4, 1, 7, 61),
+('2021-10-15', 4, 2, 7, 61),
+('2021-10-15', 4, 3, 7, 61),
+('2021-10-15', 4, 1, 8, 71),
+('2021-10-15', 4, 2, 8, 71),
+('2021-10-15', 4, 3, 8, 71);
+
+--Approves (First 6 Books)
+INSERT INTO Approves(eid, room, floor, time, date) VALUES 
+(1, 1, 1, 9, '2021-10-15'),
+(1, 2, 1, 10, '2021-10-15'),
+(1, 3, 1, 9, '2021-10-15'),
+(11, 1, 2, 13, '2021-10-15'),
+(21, 3, 3, 13, '2021-10-16'),
+(31, 1, 4, 14, '2021-10-16');
+
+--Books (11)
+INSERT INTO Books(eid, room, floor, time, date) VALUES 
+(1, 1, 1, 9, '2021-10-15'),
+(2, 2, 1, 10, '2021-10-15'),
+(3, 3, 1, 9, '2021-10-15'),
+(11, 1, 2, 13, '2021-10-15'),
+(21, 3, 3, 13, '2021-10-16'),
+(32, 1, 4, 14, '2021-10-16'),
+(33, 2, 4, 11, '2021-10-23'),
+(43, 3, 5, 9, '2021-10-24'),
+(52, 2, 6, 9, '2021-10-25'),
+(61, 1, 7, 9, '2021-10-25'),
+(72, 3, 8, 9, '2021-10-26');
+
+--Joins (46 rows)
+--Finance people joining all three rooms
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (1, 1, 1, 9, '2021-10-15');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (4, 1, 1, 9, '2021-10-15');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (5, 1, 1, 9, '2021-10-15');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (6, 1, 1, 9, '2021-10-15');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (2, 2, 1, 10, '2021-10-15');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (7, 2, 1, 10, '2021-10-15');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (3, 3, 1, 9, '2021-10-15');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (8, 3, 1, 9, '2021-10-15');
+
+--HR joining one room
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (11, 1, 2, 13, '2021-10-15');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (12, 1, 2, 13, '2021-10-15');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (13, 1, 2, 13, '2021-10-15');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (14, 1, 2, 13, '2021-10-15');
+
+--Department 3 joining 1 room
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (21, 3, 3, 13, '2021-10-16');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (23, 3, 3, 13, '2021-10-16');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (25, 3, 3, 13, '2021-10-16');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (28, 3, 3, 13, '2021-10-16');
+
+--Department 4 joining 2 rooms
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (32, 1, 4, 14, '2021-10-16');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (31, 1, 4, 14, '2021-10-16');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (35, 1, 4, 14, '2021-10-16');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (34, 1, 4, 14, '2021-10-16');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (33, 2, 4, 11, '2021-10-23');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (37, 2, 4, 11, '2021-10-23');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (38, 2, 4, 11, '2021-10-23');
+ 
+--Department 5 joining 1 room
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (43, 3, 5, 9, '2021-10-24');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (44, 3, 5, 9, '2021-10-24');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (45, 3, 5, 9, '2021-10-24');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (46, 3, 5, 9, '2021-10-24');
+ 
+--Department 6 joining 1 room
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (52, 2, 6, 9, '2021-10-25');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (53, 2, 6, 9, '2021-10-25');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (54, 2, 6, 9, '2021-10-25');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (55, 2, 6, 9, '2021-10-25');
+ 
+--Department 7 joining 1 room
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (61, 1, 7, 9, '2021-10-25');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (62, 1, 7, 9, '2021-10-25');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (63, 1, 7, 9, '2021-10-25');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (64, 1, 7, 9, '2021-10-25');
+
+--Department 8 joining 1 room after change in capacity
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (72, 3, 8, 9, '2021-10-26');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (71, 3, 8, 9, '2021-10-26');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (73, 3, 8, 9, '2021-10-26');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (74, 3, 8, 9, '2021-10-26');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (75, 3, 8, 9, '2021-10-26');
+INSERT INTO Joins (eid, room, floor, time, date) VALUES (76, 3, 8, 9, '2021-10-26');
